31627857d8f8812c6319a970650bffa54851bc8f
andreas <andreas.mansson1@ingka.ikea.com>
2020-04-08 12:30:44 +0200

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function slack() {
       throw new Error('SLACK_WEBHOOK_URL not set as a secret in github');
     }
     const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
-    let payload = eval("payload = " + core.getInput('payload'));
+    var payload = eval("payload = " + core.getInput('payload'));
 
     await webhook.send(JSON.parse(JSON.stringify(payload)));
   } catch (error) {
